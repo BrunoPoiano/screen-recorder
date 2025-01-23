3686b9871434f75fef201f50ab4823cbd5f1d28f
BrunoPoiano <bruno.poiano@proton.me>
2025-01-23 14:54:07 -0300

    generating linux x64 zip

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,13 @@
 module.exports = {
   packagerConfig: {
-    asar: true,
+    out: "release",
+    platform: ["linux"],
+    arch: ["x64"],
   },
-  rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
+      name: "@electron-forge/maker-zip",
+      platforms: ["linux"],
     },
   ],
 };
